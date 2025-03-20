7839b47a6a436862a0a2c4b99538888e16539d91
Morales Flores Axel Uriel <morales.florea.axel.uriel@gmail.com>
2025-03-20 00:59:05 -0600

    Cree la primera carpeta de nuestro proyecto

---
+++ src/lecty_read_/Lecty_Read_.java
@@ -0,0 +1,11 @@
+package lecty_read_;
+
+public class Lecty_Read_ {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola que tal :D");
+        
+    }
+    
+}
