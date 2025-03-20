63d06d1515a7fa803fc00394328e3c70e0fea151
Morales Flores Axel Uriel <morales.florea.axel.uriel@gmail.com>
2025-03-20 01:44:49 -0600

    Agregue info del equipo

--- src/lecty_read_/Lecty_Read_.java
+++ src/lecty_read_/Lecty_Read_.java
@@ -1,3 +1,17 @@
+/*
+Equipo: 4
+Nombre del Equipo: Bermel
+Participantes :
+    Araus Monciño Adrian Vladimir
+    Caballero Antonio Aura Estrella
+    Alberto Ledezma Valdez
+    Morales Flores Axel Uriel
+    Suarez Galvan Julio
+Nombre de la Esceuela: CECyT 9 "Juan de Dios Bátiz"
+Institución: Instituto Politécnico Nacional (IPN)
+Profesora: Vasquez Sánchez Alma Erika
+Materia: Programación Intermedia, Base de Datos y Laboratorio de Desarrollo de Software
+*/
 package lecty_read_;
 
 public class Lecty_Read_ {
